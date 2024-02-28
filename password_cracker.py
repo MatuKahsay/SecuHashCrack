4dae8c47a9563884bdb4b72a5fbeda6e74e27190
Matu Kahsay <108632333+matu-kahsay@users.noreply.github.com>
2024-02-27 17:05:09 -0800

    Creating password_cracker file

---
+++ password_cracker.py
@@ -0,0 +1,8 @@
+import hashlib
+import argparse
+import logging
+import time
+import requests  # Import the requests library
+import itertools
+import string
+import re  # Make sure to import the regex module
